5c4117209a7dd335528334e88ea62b4722f7dc9d
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-06-22 22:02:18 +0500

    Feat TA-01: Basic homepage design
    - Added basic homepage design with links
    - Shitty design overall lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Sandbox from "./pages/sandbox";
+import SandboxThumbnail from "./assets/thumbnails/sandbox.png";
+
+export const projectUrls = [
+  {
+    title: "Sandbox",
+    url: "/sandbox",
+    component: <Sandbox />,
+    thumbnail: SandboxThumbnail,
+  },
+];
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sandbox" element={<Sandbox />} />
+        {projectUrls.map(({ url, component }, _) => (
+          <Route key={_} path={url} element={component} />
+        ))}
       </Routes>
     </Router>
   );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import "./index.css";
+import { useNavigate } from "react-router-dom";
+import { projectUrls } from "../../App";
+import placeholderImage from "../../assets/thumbnails/placeholder.jpg";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path = "") => {
+    if (typeof path !== "string" || !path) return;
+    navigate(path);
+  };
+
   return (
-    <div>
-      <h1>THREE JS ARCHIVE</h1>
+    <div className="container-main color-white">
+      <h1 className="p-heading">THREE JS ARCHIVE</h1>
+      <div>
+        <h2 className="content-heading">Contents</h2>
+        <div className="content-wrapper">
+          {projectUrls.map((project) => (
+            <div
+              key={project.url}
+              onClick={() => handleNavigation(project.url)}
+              className="card"
+            >
+              <img
+                src={project.thumbnail ?? placeholderImage}
+                alt={project.title}
+                className="thumbnail"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
